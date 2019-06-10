926cdc33f88fdbd0fd2fc7b4a754e84050f49983
Rosmari Nikova <rosmari.nikova@yahoo.com>
2019-06-10 20:45:44 +0300

    Add new problem SoftUni Parking

---
+++ Defining Classes/SoftUniParking/Car.cs
@@ -0,0 +1,30 @@
+﻿namespace SoftUniParking
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.RegistrationNumber = registrationNumber;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+$@"Make: {Make}
+Model: {Model}
+HorsePower: {HorsePower}
+RegistrationNumber: {RegistrationNumber}");
+        }
+    }
+}
---
+++ Defining Classes/SoftUniParking/Parking.cs
@@ -0,0 +1,67 @@
+﻿namespace SoftUniParking
+{
+    using System.Collections.Generic;
+
+    public class Parking
+    {
+        private Dictionary<string, Car> cars;
+
+        private int capacity;
+
+        public Parking(int capacity)
+        {
+            this.cars = new Dictionary<string, Car>();
+            this.capacity = capacity;
+        }
+
+        public int Count => cars.Count;
+
+
+        public string AddCar(Car car)
+        {
+            if (cars.ContainsKey(car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (cars.Count == capacity)
+            {
+                return "Parking is full!";
+            }
+
+            cars.Add(car.RegistrationNumber, car);
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if (!cars.ContainsKey(registrationNumber))
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+
+            cars.Remove(registrationNumber);
+            return $"Successfully removed {registrationNumber}";
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            Car car = null;
+
+            if (cars.ContainsKey(registrationNumber))
+            {
+                car = cars[registrationNumber];
+            }
+
+            return car;
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (var registrationNumber in registrationNumbers)
+            {
+                RemoveCar(registrationNumber);
+            }
+        }
+    }
+}
