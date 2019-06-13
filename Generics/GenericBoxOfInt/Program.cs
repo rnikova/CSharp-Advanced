bcbeae8833cf5d7e388614d69336e58a2eaec693
Rosmari Nikova <rosmari.nikova@yahoo.com>
2019-06-13 21:53:45 +0300

    Add new problems

---
+++ Generics/GenericBoxOfInt/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GenericBoxOfString
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int linesCount = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                Box<int> box = new Box<int>(input);
+
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
