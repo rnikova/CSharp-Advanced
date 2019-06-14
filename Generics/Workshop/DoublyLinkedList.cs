93df6083f33d90ef877715fe7e6afb5594f72518
Rosmari Nikova <rosmari.nikova@yahoo.com>
2019-06-15 00:35:10 +0300

    Add new problem

---
+++ Generics/Workshop/DoublyLinkedList.cs
@@ -0,0 +1,97 @@
+﻿namespace Workshop
+{
+    using System;
+
+    public class DoublyLinkedList
+    {
+        private class ListNode
+        {
+            public int Value { get; }
+
+            public ListNode NextNode { get; set; }
+
+            public ListNode PreviousNode { get; set; }
+
+            public ListNode(int value)
+            {
+                this.Value = value;
+            }
+        }
+
+        private ListNode head;
+        private ListNode tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(int value)
+        {
+            ListNode newHead = new ListNode(value);
+
+            if (this.Count == 0)
+            {
+                this.head = this.tail = newHead;
+            }
+            else
+            {
+                newHead.NextNode = this.head;
+                this.head.PreviousNode = newHead;
+                this.head = newHead;
+            }
+
+            this.Count++;
+        }
+
+        public void AddLast(int value)
+        {
+            ListNode newTail = new ListNode(value);
+
+            if (this.Count == 0)
+            {
+                this.head = this.tail = newTail;
+            }
+            else
+            {
+                newTail.PreviousNode = this.tail;
+                this.tail.NextNode = newTail;
+                this.tail = newTail;
+            }
+
+            this.Count++;
+        }
+
+        public int RemoveFirst()
+        {
+            if (this.Count==0)
+            {
+                throw new InvalidOperationException("DoublyLinkedList is empty!");
+            }
+
+            int fisrtElement = this.head.Value;
+            this.head = this.head.NextNode;
+
+            if (this.head==null)
+            {
+                this.tail = null;
+            }
+            else
+            {
+                this.head.PreviousNode = null;
+            }
+
+            this.Count--;
+
+            return fisrtElement;
+        }
+
+        public void Print(Action<int> action)
+        {
+            ListNode currentNode = this.head;
+
+            while (currentNode!=null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }
+        }
+    }
+}
---
+++ Generics/Workshop/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Workshop
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doublyLinkedList = new DoublyLinkedList();
+
+            //doublyLinkedList.AddFirst(1);
+            //doublyLinkedList.AddFirst(2);
+            //doublyLinkedList.AddFirst(3);
+
+            //doublyLinkedList.Print(Console.WriteLine);
+            //Console.WriteLine(doublyLinkedList.Count==3);
+
+            doublyLinkedList.AddLast(1);
+            doublyLinkedList.AddLast(2);
+            doublyLinkedList.AddLast(3);
+
+            doublyLinkedList.RemoveFirst();
+
+            Console.WriteLine(doublyLinkedList.Count == 2);
+            doublyLinkedList.Print(Console.WriteLine);
+        }
+    }
+}
