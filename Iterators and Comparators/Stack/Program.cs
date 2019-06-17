72a714372eccbd08dff8d253468d48c0b3747762
Rosmari Nikova <rosmari.nikova@yahoo.com>
2019-06-17 23:33:16 +0300

    Add new homework problem

---
+++ Iterators and Comparators/Stack/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Stack
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            CustomStack<int> stack = new CustomStack<int>();
+
+            while (command != "END")
+            {
+                string[] commands = command.Split(" ", 2);
+
+                if (commands[0] == "Push")
+                {
+                    int[] numbers = commands[1].Split(", ").Select(int.Parse).ToArray();
+
+                    stack.Push(numbers);
+                }
+                else
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var number in stack)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
